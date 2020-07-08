17c5a56c24738e9d466fbdf5a918fe45025cb518
shlande <shlandeme@gmail.com>
2020-07-08 11:43:35 +0800

    未完成的代码

--- tree.go
+++ tree.go
@@ -13,7 +13,7 @@ func (n *node) add(httpMethod string, handlerFunc HandlerFunc) {
 	n.handler[httpMethod] = handlerFunc
 }
 
-func (n *node) match(method string) HandlerFunc {
+func (n *node) match(method string, args []string) HandlerFunc {
 	var (
 		has    bool
 		handle HandlerFunc
@@ -24,6 +24,7 @@ func (n *node) match(method string) HandlerFunc {
 		}
 		return bodyWriter(default404Body)
 	}
+	// 在这里包一层用于路由参数处理
 	return handle
 }
 
@@ -38,7 +39,10 @@ type tree struct {
 }
 
 func (t *tree) addRouter(httpMethod, path string, handler HandlerFunc) {
-	n, has := t.nodes[path]
+	// 获取不包含参数的路径
+	basePath := getBasePath(path)
+	// 如果存在处理函数，那么进行多层包装
+	n, has := t.nodes[basePath]
 	if !has {
 		handler := make(map[string]HandlerFunc)
 		n = &node{
@@ -46,8 +50,58 @@ func (t *tree) addRouter(httpMethod, path string, handler HandlerFunc) {
 			handler: handler,
 		}
 	}
+	handler = n.wrapHandler(handler, n.handler[httpMethod], matcher)
 	n.add(httpMethod, handler)
 	t.nodes[path] = n
+}
+
+// 在添加路由的时候去除的参数
+func getBasePath(path string) string {
+
+}
+
+// 在添加路由的时候获取参数
+func getArgsPath(path string) string {
+	basePath := getBasePath(path)
+	if path == basePath {
+		return ""
+	}
+	return path[len(basePath)+1:]
+}
+
+// 通过可变参数的形势获取match函数
+func matchArgsTemplate(args []string, template string) bool {
+	if template == "" {
+		return args == nil
+	}
+	if template == "/*" {
+		return true
+	}
+	temps := strings.Split(template[1:], "/")
+	return len(temps) == len(args)
+}
+
+func (n *node) wrapHandler(wrapper, in HandlerFunc, argsTemplate string) HandlerFunc {
+	return func(ctx *Context) {
+		args := n.getArgs(ctx.Request.RequestURI)
+		if matchArgsTemplate(args, argsTemplate) {
+			// 设置路由参数
+
+			wrapper(ctx)
+		}
+		if in == nil {
+			in = bodyWriter(default404Body)
+		}
+		in(ctx)
+	}
+}
+
+func (n *node) getArgs(s string) []string {
+	s = s[len(n.path):]
+	if s == "" {
+		return nil
+	}
+	return strings.Split(s[1:], "/")
 }
 
 func (t *tree) match(path, method string) HandlerFunc {
@@ -56,6 +110,7 @@ func (t *tree) match(path, method string) HandlerFunc {
 		has bool
 	)
 	path = parsePath(path)
+	// 便利，直到找到node
 	if n, has = t.nodes[path]; !has {
 		return bodyWriter(default404Body)
 	}
